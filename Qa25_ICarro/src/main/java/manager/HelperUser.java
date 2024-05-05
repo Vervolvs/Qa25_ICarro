cb76b9c1c4197bfd67ff61a5a5f47af935613a1a
Vervolvs <Object_raw@mail.ru>
2024-05-05 18:38:56 +0300

    HW_5

--- Qa25_ICarro/src/main/java/manager/HelperUser.java
+++ Qa25_ICarro/src/main/java/manager/HelperUser.java
@@ -1,6 +1,8 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase {
 
@@ -8,4 +10,66 @@ public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+
+    public void openLoginRegistrationForm(){
+
+        WebElement loginTab = wd.findElement(By.cssSelector("a[href='/login?url=%2Fsearch']"));
+        loginTab.click();
+
+
+
+    }
+
+    public void fillLoginRegistrationForm(String email, String password) {
+
+        //    WebElement emailInput = wd.findElement(By.name("email"));
+        //   emailInput.click();
+        //   emailInput.clear();
+        //    emailInput.sendKeys(email);
+
+        type(By.id("email"),email);
+
+        //   WebElement passwordInput = wd.findElement(By.xpath("//input[last()]"));
+        //   passwordInput.click();
+        //   passwordInput.clear();
+        //   passwordInput.sendKeys(password);
+
+        type(By.id("password"),password);
+
+
+
+    }
+
+
+    public void submitLogin(){
+        click(By.xpath("//button[text()='Login']"));
+
+
+
+
+
+
+    }
+
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//button[text()='Sign Out']"));
+    }
+
+    public void logout() {
+
+        click(By.xpath("//button[text()='SignOut']"));
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
--- Qa25_ICarro/src/test/java/tests/LoginTests.java
+++ Qa25_ICarro/src/test/java/tests/LoginTests.java
@@ -1,14 +1,27 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
-    @Test
-    public void test(){
+    @BeforeMethod
+    public void preConditions() {
+        //if SignOut is present -----> logout
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
+
+    }
 
 
 
+
+        @Test
+    public void loginSuccess(){
+
+    app.getHelperUser().openLoginRegistrationForm();
+    app.getHelperUser().fillLoginRegistrationForm("object_raw@mail.ru","D07i03m95a!");
 
 
     }
